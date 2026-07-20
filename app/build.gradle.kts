25dc55602a09fea63907c830d84e36b43657f97b
Manas <saparbektv@gmail.com>
2026-07-20 19:36:10 +0500

    Update Kotlin and Java version compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,16 +20,16 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    
     buildFeatures {
         compose = true
     }
+    
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.8"
     }
